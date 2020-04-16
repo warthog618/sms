7c4498631b1eb704fb2c44c7454480b4b4342823
Kent Gibson <warthog618@gmail.com>
2020-04-16 11:06:34 +0800

    move pdumode into encoding

--- ms/doc.go
+++
@@ -1,8 +0,0 @@
-/// SPDX-License-Identifier: MIT
-//
-// Copyright © 2018 Kent Gibson <warthog618@gmail.com>.
-
-// Package ms provides functionality specific to the Mobile Station.
-//
-//  - pdumode provides encoding and decoding of PDU mode frames from SMS modems.
-package ms
